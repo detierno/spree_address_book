98b3f0f7469626bb9a09b3c6965fd6220667d933
Denis Tierno <de.tierno@gmail.com>
2011-07-06 11:00:19 -0700

    corrected to_s override method via Github

--- app/models/address_decorator.rb
+++ app/models/address_decorator.rb
@@ -11,7 +11,7 @@ Address.class_eval do
   end
   
   def to_s
-    "#{firstname} #{lastname}: #{zipcode}, #{country}, #{state || state_name}, #{city}, #{address1} #{address2}"
+    "<p class=\"address_name\">{firstname} #{lastname}<p> <p>#{address1} #{address2}</p> <p>#{city} - #{state || state_name}</p> <p>CEP: #{zipcode}, #{country}</p>"
   end
   
   def destroy_with_saving_used
